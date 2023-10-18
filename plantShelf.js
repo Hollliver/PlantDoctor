7070983e34b35de69da43354b3d82f3496827b82
holly norman <holly3norman@gmail.com>
2023-10-18 23:31:19 +0100

    working image carousel-test images

--- plantShelf.js
+++ plantShelf.js
@@ -1,40 +1,33 @@
-// initialise array to contain slides
+
+// change slide on next and previous button click
 let slides = document.getElementsByClassName("slider__slide");
-// array storing breadcrumbs
- let navlinks = document.getElementsByClassName("slider__navlink");
-// which slide is active
+let navlinks = document.getElementsByClassName("slider__navlink");
 let currentSlide = 0;
 
+document.getElementById("next").addEventListener("click", () => {changeSlide(currentSlide + 1)});
+document.getElementById("previous").addEventListener("click", () => {changeSlide(currentSlide - 1)});
 
-// event listeners for nav buttons
-document.getElementsById("next").addEventListener("click", () => {
-    changeSlide(currentSlide + 1)
-});
-document.getElementById("previous").addEventListener("click", () => {
-    changeSlide(currentSlide - 1)
-});
-
-
-// function to change slides
 function changeSlide(moveTo) {
-    if (moveTo >= slides.length) {moveTo = 0;}
-    if (moveTo < 0) {moveTo = slides.length - 1;}
-    
+    if(moveTo >= slides.length) {
+        moveTo = 0;
+    }
+
+    if(moveTo < 0) {
+        moveTo = slides.length - 1;
+    }
+
     slides[currentSlide].classList.toggle("active");
     navlinks[currentSlide].classList.toggle("active");
     slides[moveTo].classList.toggle("active");
     navlinks[moveTo].classList.toggle("active");
-    
+
     currentSlide = moveTo;
 }
-
-
-// breadcrumb function
+// click breadcrumbs to change slide
 document.querySelectorAll('.slider__navlink').forEach((bullet, bulletIndex) => {
     bullet.addEventListener('click', () => {
-        if (currentSlide !== bulletIndex) {
+        if(currentSlide !== bulletIndex) {
             changeSlide(bulletIndex);
         }
     })
 })
-
