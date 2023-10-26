54386e5d5b70caa7ab6416e58b979a00725d62f2
holly norman <holly3norman@gmail.com>
2023-10-26 21:24:22 +0100

    added: images + functional carousel text

--- plantShelf.js
+++ plantShelf.js
@@ -1,8 +1,10 @@
 // index.html
 // CAROUSEL
 // change slide on next and previous button click
+/*
 let slides = document.getElementsByClassName("slider__slide");
 let navlinks = document.getElementsByClassName("slider__navlink");
+let slideText = document.getElementsByClassName("slide_text");
 let currentSlide = 0;
 
 document.getElementById("next").addEventListener("click", () => {changeSlide(currentSlide + 1)});
@@ -19,8 +21,10 @@ function changeSlide(moveTo) {
 
     slides[currentSlide].classList.toggle("active");
     navlinks[currentSlide].classList.toggle("active");
+    slideText[currentSlide].classList.toggle("active");
     slides[moveTo].classList.toggle("active");
     navlinks[moveTo].classList.toggle("active");
+    slideText[moveTo].classList.toggle("active");
 
     currentSlide = moveTo;
 }
@@ -32,6 +36,42 @@ document.querySelectorAll('.slider__navlink').forEach((bullet, bulletIndex) => {
         }
     })
 })
+*/
+// Change Carousel Images
+let slides = document.querySelectorAll('.slider__slide');
+let slideText = document.querySelectorAll('.slide_text');
+let navLinks = document.querySelectorAll('.slider__navlink');
+let currentSlide = 0;
+
+// next button
+document.getElementById('next').addEventListener('click', () => {
+changeSlide(currentSlide + 1);
+});
+// previous button
+document.getElementById('previous').addEventListener('click', () => {
+    changeSlide(currentSlide - 1);
+});
+
+function changeSlide(moveTo) {
+    slides[currentSlide].classList.remove('active');
+    slideText[currentSlide].classList.remove('active');
+    navLinks[currentSlide].classList.remove('active');
+    currentSlide = (moveTo + slides.length) % slides.length;
+    slides[currentSlide].classList.add('active');
+    slideText[currentSlide].classList.add('active');
+    navLinks[currentSlide].classList.add('active');
+}
+
+// Change breadcrumbs
+document.querySelectorAll('.slider__navlink').forEach((bullet, bulletIndex) => {
+    bullet.addEventListener('click', () => {
+        if(currentSlide !== bulletIndex) {
+            changeSlide(bulletIndex);
+        }
+    })
+})
+
+
 
 // Scroll to element
  function scrollToElement(targetId) {
