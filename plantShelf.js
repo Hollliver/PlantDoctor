ca4d532871ce7d19420df78407ebdff9afd83b55
holly norman <holly3norman@gmail.com>
2023-11-02 22:21:31 +0000

    temp fix to add plant submit button - moved top right

--- plantShelf.js
+++ plantShelf.js
@@ -1,42 +1,5 @@
 // index.html
-// CAROUSEL
-// change slide on next and previous button click
-/*
-let slides = document.getElementsByClassName("slider__slide");
-let navlinks = document.getElementsByClassName("slider__navlink");
-let slideText = document.getElementsByClassName("slide_text");
-let currentSlide = 0;
 
-document.getElementById("next").addEventListener("click", () => {changeSlide(currentSlide + 1)});
-document.getElementById("previous").addEventListener("click", () => {changeSlide(currentSlide - 1)});
-
-function changeSlide(moveTo) {
-    if(moveTo >= slides.length) {
-        moveTo = 0;
-    }
-
-    if(moveTo < 0) {
-        moveTo = slides.length - 1;
-    }
-
-    slides[currentSlide].classList.toggle("active");
-    navlinks[currentSlide].classList.toggle("active");
-    slideText[currentSlide].classList.toggle("active");
-    slides[moveTo].classList.toggle("active");
-    navlinks[moveTo].classList.toggle("active");
-    slideText[moveTo].classList.toggle("active");
-
-    currentSlide = moveTo;
-}
-// click breadcrumbs to change slide
-document.querySelectorAll('.slider__navlink').forEach((bullet, bulletIndex) => {
-    bullet.addEventListener('click', () => {
-        if(currentSlide !== bulletIndex) {
-            changeSlide(bulletIndex);
-        }
-    })
-})
-*/
 // Change Carousel Images
 let slides = document.querySelectorAll('.slider__slide');
 let slideText = document.querySelectorAll('.slide_text');
